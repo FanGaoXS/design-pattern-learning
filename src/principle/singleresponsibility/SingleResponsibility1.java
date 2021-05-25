5fb945dc8fbc13f9d7e8025641c83f8833ca29d8
FanGaoXS <954278478@qq.com>
2021-05-25 18:32:14 +0800

    design@wqk 新增单一职责原则和接口隔离原则

---
+++ src/principle/singleresponsibility/SingleResponsibility1.java
@@ -0,0 +1,11 @@
+package principle.singleresponsibility;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: 吴青珂
+ * @Date: 2021/05/25/16:32
+ * @Description:
+ */
+public class SingleResponsibility1 {
+}
